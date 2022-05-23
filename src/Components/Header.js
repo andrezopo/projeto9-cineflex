68525b9c7ea712169c9d64f645bdd30fcceeb77d
André Zopolato <andrezopo@gmail.com>
2022-05-22 21:36:30 -0300

    bônus botão voltar concluído

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,20 @@
+import { useNavigate, useLocation } from "react-router-dom";
 import styled from "styled-components";
+import Button from "./DefaultButton";
 
 export default function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
-    <>
+    <div>
       <HeaderStyle>CINEFLEX</HeaderStyle>
-    </>
+      {location.pathname === "/" ? null : (
+        <Button onClick={() => navigate(-1)} width={"60px"}>
+          Voltar
+        </Button>
+      )}
+    </div>
   );
 }
 
@@ -12,6 +22,7 @@ const HeaderStyle = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  text-align: center;
   font-size: 34px;
   font-weight: 400;
   color: #e8833a;
